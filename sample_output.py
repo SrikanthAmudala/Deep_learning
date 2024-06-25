b0abba5282a72d00166d48adb32163f81481c22c
Srikanth Amudala <srikanthamudala95@gmail.com>
2024-06-25 01:32:45 -0700

    Tested Donut to read data from IMGs, rating 2

---
+++ sample_output.py
@@ -0,0 +1,40 @@
+{'menu': [
+        {
+            'nm': 'Company Sample Invoice', 
+            'num': '06/10/2021', 
+            'price': 'INVO-005'
+        },
+
+        {
+            'nm': 'Billing Information', 
+            'num': 'Shipping Information'
+        },
+
+        {
+            'nm': '11 Pine Street Suite 1815', 
+            'num': 'San Francisco, CA, 94111', 'price': '1111'
+        },
+        {
+            'nm': 'Phone Number', 
+            'num': '(123) 123-1232', 
+            'price': '(RIA)'
+        }
+    ], 
+    
+    'sub_total': 
+    {
+        'subtotal_price': {
+            'price': 'Total'
+        }, 
+        'discount_price': '$200', 
+        'service_price': '$150', 
+        'tax_price': '$50', 
+        'etc': '1'
+    }, 
+    
+    'total': {
+        'total_price': '25', 
+        'emoneyprice': '$425', 
+        'menuqty_cnt': '4'
+    }
+}
